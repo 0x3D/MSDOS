456ad2d2b517f5489829bf95ee2ae04d9b672ca7
axelhertzberg <axelher@student.chalmers.se>
2021-04-12 12:56:17 +0200

    folders

--- msdos/src/App.js
+++ msdos/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './styles/App.css';
 
 function App() {
   document.title = 'MSDOS Booking'
   return (
     <div className="App">
-    <h2> MSDOS </h2> 
+      <h2> MSDOS </h2> 
+      <h3> HEJ HEJ HEJ </h3>
     </div>
   );
 }
