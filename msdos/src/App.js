1b345b160e08f94c2a5c7df3a521dc0655946a46
axelhertzberg <axelher@student.chalmers.se>
2021-04-13 17:36:42 +0200

    La till en Reactcomponent, syfte: att jag gjorde oika 'npm install' för att se om ni får beroenderna direkt

--- msdos/src/App.js
+++ msdos/src/App.js
@@ -1,13 +1,15 @@
 import './styles/App.css';
+import Testcomponent from "./components/Testcomponent"
+
 
 function App() {
   document.title = 'MSDOS Booking'
   return (
     <div className="App">
       <h2> MSDOS </h2> 
-      <h3>HEJ HEJ</h3>
-      <h3> BLABLABLA</h3>
+      <Testcomponent />
     </div>
+
   );
 }
 
