dc66aa2a2237b9dccb9f98d5ed55986a2c13665d
jonasn-chalmers <jonnordi@student.chalmers.se>
2021-04-14 14:42:59 +0200

    Set default to homepage

--- msdos/src/App.js
+++ msdos/src/App.js
@@ -1,7 +1,7 @@
 import './styles/App.css';
 import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './Home';
 import Test from './Test';
 import NavigationBar from './components/NavigationBar'
@@ -17,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/home" component={Home} />
           <Route path="/test" component={Test} />
+          <Redirect to="/home"/>
         </Switch>
       </Router>
     </>
