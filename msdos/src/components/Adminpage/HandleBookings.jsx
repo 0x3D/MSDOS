37f4af28f2ea2f2bd8ac7ce41fa3f7efc45194b0
axelhertzberg <axelher@student.chalmers.se>
2021-04-15 17:11:12 +0200

    add some things on adminpage

--- msdos/src/components/Adminpage/HandleBookings.jsx
+++ msdos/src/components/Adminpage/HandleBookings.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import '../../styles/AdminPage.css'
+import { makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core/'
+
+var jsonTestData = require('./../../testData/handleBookings.json')
 
 const useStyles = makeStyles({
   table: {
@@ -16,9 +10,8 @@ const useStyles = makeStyles({
 });
 
 function createData(name, type, time, date) {
-  return { name, type, time, date};
+  return { name, type, time, date };
 }
-
 
 const rows = [
   createData('test1', 'tvätt', 13, "tors"),
@@ -28,37 +21,36 @@ const rows = [
   createData('testd5', 'tvätt', 10, "lördag"),
 ];
 
-
 export default function HandleBookings() {
 
- const classes = useStyles()
+  const classes = useStyles()
 
   return (
     <div>
       <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow style ={{backgroundColor:"GrayText"}}>
-            <TableCell align="left"><h3>Name</h3></TableCell>
-            <TableCell align="center"><h3>Faciliteter</h3></TableCell>
-            <TableCell align="center"><h3>Tid</h3></TableCell>
-            <TableCell align="center"><h3>Dag</h3></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.type}</TableCell>
-              <TableCell align="center">{row.time}</TableCell>
-              <TableCell align="center">{row.date}</TableCell>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow style={{ backgroundColor: "LightGrey" }}>
+              <TableCell align="left"><h3>Name</h3></TableCell>
+              <TableCell align="center"><h3>Faciliteter</h3></TableCell>
+              <TableCell align="center"><h3>Tid</h3></TableCell>
+              <TableCell align="center"><h3>Dag</h3></TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.type}</TableCell>
+                <TableCell align="center">{row.time}</TableCell>
+                <TableCell align="center">{row.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
--- msdos/src/components/Adminpage/HandleUsers.jsx
+++ msdos/src/components/Adminpage/HandleUsers.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import '../../styles/AdminPage.css';
-
+import {makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from '@material-ui/core/'
 
 const useStyles = makeStyles({
   table: {
@@ -39,7 +30,7 @@ const classes = useStyles()
       <TableContainer component={Paper} >
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow style ={{backgroundColor:"GrayText"}}>
+          <TableRow style ={{backgroundColor:"LightGrey"}}>
             <TableCell ><h2>Name</h2></TableCell>
             <TableCell align="center"><h2>Lägenhetsnummer</h2></TableCell>
             <TableCell align="center"><h2>Mobilnummer</h2></TableCell>
