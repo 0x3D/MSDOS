c9c988e0febe220147292731174a3684d9439841
0x3D <0x3D@users.noreply.github.com>
2021-04-19 17:41:38 +0200

    Added validation for users in Users
    Have to generate tokens when login still

--- src/LoginBackend.js
+++ src/LoginBackend.js
@@ -1,30 +1,40 @@
 // Backend 
 
 import { localStorageAvailable } from "@material-ui/data-grid";
+import Users from "./testData/users.json"
 
 export default class LoginBackend {
-    getLoginToken(apartment, password){
+    getLoginToken(apartment, password) {
         // TODO: Json webtokens and/or Argon2 // Ask Erik B or Erik R
         return apartment + password;
     }
 
     basic_login(apartmentNo, password) {
         const token = this.getLoginToken(apartmentNo, password);
-    
+
         // Save cookie if browser allows. 
-        if (localStorageAvailable){
-            localStorage.setItem('loginToken',token);
+        if (localStorageAvailable) {
+            localStorage.setItem('loginToken', token);
         }
-        return true; 
+        return true;
     }
 
     authenticate_token(token) {
-        if( token !== null && token.length > 1){
-            return true
-        } else {
-            return false;
-        }
+        // TODO: make this pretty 
+        let found = false;
+        Users.map((user) => {
+            console.log(user.password);
+            console.log(token)
+            console.log(user.password === token);
+            if (user.password.valueOf() === token.valueOf()) {
+                found = true; 
+            }
+        })
+        return found;
+        //if (token !== null && token.length > 1) {
+        //    return true
+        //} else {
+        //    return false;
+        //}
     }
-
-
 }
