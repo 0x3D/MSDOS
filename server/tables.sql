abdc40d2e955a32fb1e87f6fec16a0d66f9c4a02
0x3D <0x3D@users.noreply.github.com>
2021-04-21 19:35:07 +0200

    add email constraint unique apartmentno

--- server/tables.sql
+++ server/tables.sql
@@ -1,13 +1,14 @@
 --- Insert tables into postgres 
 CREATE TABLE Logins(
     email TEXT PRIMARY KEY,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    CONSTRAINT proper_email CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
 );
 
 CREATE TABLE Users(
-    apartmentNo INTEGER NOT NULL,
+    apartmentNo INTEGER NOT NULL UNIQUE,
     email TEXT PRIMARY KEY,
-    FOREIGN KEY (email) REFERENCES Logins(email)
+    FOREIGN KEY (email) REFERENCES Logins(email),
 );
 
 CREATE TABLE Admins(
