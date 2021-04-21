adf89dbc5be697331f494c1eb7c4410aaecbe234
0x3D <0x3D@users.noreply.github.com>
2021-04-21 19:14:04 +0200

    Fix tables.sql

--- server/tables.sql
+++ server/tables.sql
@@ -2,15 +2,16 @@
 CREATE TABLE Logins(
     email TEXT PRIMARY KEY,
     password TEXT NOT NULL
-)
+);
 
 CREATE TABLE Users(
     apartmentNo INTEGER NOT NULL,
     email TEXT PRIMARY KEY,
+    FOREIGN KEY (email) REFERENCES Logins(email)
 );
 
 CREATE TABLE Admins(
     email TEXT PRIMARY KEY,
-    FOREIGN KEY (email) REFERENCES Users(email)
+    FOREIGN KEY (email) REFERENCES Logins(email)
 );
 
