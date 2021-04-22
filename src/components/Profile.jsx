2eb044558a47bf17b1f7efa72f54754b2f3e7eb1
axelhertzberg <axelher@student.chalmers.se>
2021-04-22 18:41:33 +0200

    profile render own bookings done, just need the loginUser

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,53 +1,76 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
 
 export default function Profile() {
 
+    /**
+     * TODO: När inloggningen är klar måste ni skicka vilket lägenhetsnummer 
+     * som är inloggad
+     */
 
-    const userName = "Håkan Håkansson"
-    const location = "Lemansgatan 6"
-    const lghNr = 7
-    const bookings = [
-        {
-            start_time: "2021-04-21 08:00:00",
-            end_time: "2021-04-21 11:00:00"
-        },
-        {
-            start_time: "2021-06-21 11:00:00",
-            end_time: "2021-06-21 14:00:00"
-        }
-    ]
+    const currentUser = 3
+
+    const formatLghNr = () => {
+        return "lghNr=" + String(3)
+    }
+
+    const [userData, setUserData] = useState(null)
+    const [laundryBookings, setLaundryBookings] = useState(null)
+
+    const fetchUsers = async () => {
+        const response = await fetch('http://localhost:8000/users?' + formatLghNr())
+        const data = await response.json()
+        setUserData(data)
+        console.log(data)
+    }
+
+    const fetchBookings = async () => {
+        const response = await fetch('http://localhost:8000/laundryBookings?' + formatLghNr())
+        const data = await response.json()
+        setLaundryBookings(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        fetchBookings()
+        fetchUsers()
+    }, [])
+
 
     return (
         <div>
+            {/* {laundryBookings ?  (laundryBookings[0].start_time) : <p>ERROR</p>} */}
             <Container>
                 <Row>
                     <Col>
                         <h3>
-                            <b>Namn: </b>{userName}
+                            <h3><b>Email: </b></h3> {!userData ? (<h2>Not logged in</h2>) : 
+                            (userData[0].email)}
                         </h3>
                     </Col>
+
                     <Col>
                         <h3>
-                            <h3><b>Lägenhetsnummer: </b></h3> {lghNr}
+                            <h3><b>Lägenhetsnummer: </b></h3> {currentUser}
                         </h3>
                     </Col>
                 </Row>
-                {!bookings ? (<h1>loading...</h1>) : (
+                {!laundryBookings ? (<h1>loading...</h1>) : (
                     <Card style={{}}>
-                        <Card.Header as="h3"> <b>Mina Bokningar</b> </Card.Header> <br/>
-                        {bookings.map((row) => (
+                        <Card.Header as="h3"> <b>Mina Bokningar</b> </Card.Header> <br />
+                        {laundryBookings.map((row) => (
                             <>
                                 <Card.Text className="border">
-                                    <b>StartTime</b> : {row.start_time} <br/> <b>Sluttid</b> : {row.end_time} <br/>
+                                    <b>StartTime</b> : {row.start_time} <br /> <b>Sluttid</b> : {row.end_time} <br />
                                     <Button variant="danger">Ta bort bokning</Button>
                                 </Card.Text>
-                                
+
                             </>
                         ))}
                     </Card>
                 )}
-                 </Container>
+            </Container>
         </div>
     )
 }
