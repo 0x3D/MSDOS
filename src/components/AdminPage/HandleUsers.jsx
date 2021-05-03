245ac8fc93ca3895ec346e503de5ab44fce6fa99
erikbengtsson <eribeng@student.chalmers.se>
2021-05-03 11:06:35 +0200

    added icons to adminpage

--- src/components/AdminPage/HandleUsers.jsx
+++ src/components/AdminPage/HandleUsers.jsx
@@ -1,7 +1,17 @@
-import React, { useState, useEffect } from 'react'
-import { Button, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core/'
-import { Container, Row, Col, Toast } from 'react-bootstrap'
-import CheckBox from '../../assets/greenCheck.png'
+import React, { useState, useEffect } from "react";
+import {
+  Button,
+  makeStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@material-ui/core/";
+import { Container, Row, Col, Toast } from "react-bootstrap";
+import { FaCheck } from "react-icons/fa";
 
 /**
  * @constant useStyles is used to set the width of the table created
@@ -9,10 +19,9 @@ import CheckBox from '../../assets/greenCheck.png'
  */
 const useStyles = makeStyles({
   table: {
-    minWidth: 650
-  }
-}
-)
+    minWidth: 650,
+  },
+});
 
 /**
  * A admin component that gives a overview of all Users
@@ -23,7 +32,7 @@ const useStyles = makeStyles({
  * @version 0.1.0
  * @author [Axel Hertzberg](https://github.com/axelhertzberg)
  */
-export default function HandleUsers () {
+export default function HandleUsers() {
   /**
    * users is a variables, and setUsers is a set-method for the variable
    * Usestate is the default value
@@ -31,50 +40,52 @@ export default function HandleUsers () {
    * @method setUsers
    * @see [reactjs](https://reactjs.org/docs/hooks-state.html)
    */
-  const [users, setUsers] = useState(null)
+  const [users, setUsers] = useState(null);
 
   /**
    * @constant  showToast is a variables, and @method setShowToast is a set-method for the variable
    * Usestate is the default value
    * @see [reactjs](https://reactjs.org/docs/hooks-state.html)
    */
-  const [showToast, setShowToast] = useState(false)
-  const toggleShowToast = () => { setShowToast(!showToast) }
+  const [showToast, setShowToast] = useState(false);
+  const toggleShowToast = () => {
+    setShowToast(!showToast);
+  };
 
   /**
    * @constant classes is to set the styles in the returned Component
    */
-  const classes = useStyles()
+  const classes = useStyles();
 
   /**
    * Fething the users data
    */
   const fetchUsers = async () => {
-    const response = await fetch('http://localhost:8000/users')
-    const data = await response.json()
-    setUsers(data)
-  }
+    const response = await fetch("http://localhost:8000/users");
+    const data = await response.json();
+    setUsers(data);
+  };
 
   /**
- * useEffect is a React function that is used to not rerender uneccesary thing
- */
+   * useEffect is a React function that is used to not rerender uneccesary thing
+   */
   useEffect(() => {
-    fetchUsers()
-  }, [])
+    fetchUsers();
+  }, []);
 
   const removeUser = async (e) => {
-    console.log('m called')
-    const id = String(e)
-    fetch('http://localhost:8000/users/' + id, {
-      method: 'DELETE',
+    console.log("m called");
+    const id = String(e);
+    fetch("http://localhost:8000/users/" + id, {
+      method: "DELETE",
       headers: {
-        'Content-type': 'application/json'
-      }
+        "Content-type": "application/json",
+      },
     })
-      .then(res => res.json())
-      .then(res => console.log(res))
-    toggleShowToast()
-  }
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+    toggleShowToast();
+  };
 
   return (
     <div>
@@ -83,52 +94,64 @@ export default function HandleUsers () {
           <Col md={{ span: 4, offset: 4 }}>
             <Toast show={showToast} onClose={toggleShowToast}>
               <Toast.Header>
-                <img width='35px' src={CheckBox} alt='' />
-                <strong className='mr-auto'>Bokning borttagen</strong>
-
+                <FaCheck size="2em" />
+                <strong className="mr-auto">Användare borttagen</strong>
               </Toast.Header>
-              <Toast.Body> Användaren har blivit borttagen! Uppdatera sidan för att se resultat </Toast.Body>
+              <Toast.Body>
+                {" "}
+                Användaren har blivit borttagen! Uppdatera sidan för att se
+                resultat{" "}
+              </Toast.Body>
             </Toast>
           </Col>
         </Row>
       </Container>
       <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label='simple table'>
+        <Table className={classes.table} aria-label="simple table">
           <TableHead>
-            <TableRow style={{ backgroundColor: 'LightGrey' }}>
-              <TableCell><h2>Lägenhetsnummer</h2></TableCell>
-              <TableCell align='center'><h2>Email</h2></TableCell>
-              <TableCell align='center'><h2> {/** SKA VARA TOM */} </h2></TableCell>
+            <TableRow style={{ backgroundColor: "LightGrey" }}>
+              <TableCell>
+                <h2>Lägenhetsnummer</h2>
+              </TableCell>
+              <TableCell align="center">
+                <h2>Email</h2>
+              </TableCell>
+              <TableCell align="center">
+                <h2> {/** SKA VARA TOM */} </h2>
+              </TableCell>
             </TableRow>
           </TableHead>
 
           <TableBody>
-            {!users
-              ? (<h1>loading...</h1>)
-              : (
-                <>
-                  {users.map((row) => (
-                    <TableRow key={row.apartmentNo}>
-                      <TableCell align='center' scope='row'>
-                        {row.apartmentNo}
-                      </TableCell>
-                      <TableCell align='center'>
-                        {row.email}
-                      </TableCell>
-                      <TableCell align='center'>
-                        <Button
-                          variant='contained' color='secondary' onClick={(e) => {
-                            removeUser(row.id)
-                          }}
-                        > Ta bort användare
-                        </Button>
-                      </TableCell>
-                    </TableRow>))}
-                </>
-                )}
+            {!users ? (
+              <h1>loading...</h1>
+            ) : (
+              <>
+                {users.map((row) => (
+                  <TableRow key={row.apartmentNo}>
+                    <TableCell align="center" scope="row">
+                      {row.apartmentNo}
+                    </TableCell>
+                    <TableCell align="center">{row.email}</TableCell>
+                    <TableCell align="center">
+                      <Button
+                        variant="contained"
+                        color="secondary"
+                        onClick={(e) => {
+                          removeUser(row.id);
+                        }}
+                      >
+                        {" "}
+                        Ta bort användare
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </>
+            )}
           </TableBody>
         </Table>
       </TableContainer>
     </div>
-  )
+  );
 }
