a5c24c530ce01e7c3e70fa92995b8b51e0b42bb7
0x3D <0x3D@users.noreply.github.com>
2021-05-04 17:22:20 +0200

    fix standard

--- src/components/Bookings/GymBooking.js
+++ src/components/Bookings/GymBooking.js
@@ -5,235 +5,231 @@ import { Button, Modal } from 'react-bootstrap'
 import '../../styles/App.css'
 import Emailer from '../../Emailer'
 
-
 /**
  * The react subtab component for booking the gym.
- * 
+ *
  * @returns The HTML to be rendered
  */
-export default function GymBooking() {
-    /**
+export default function GymBooking () {
+  /**
      * Time in minutes for one gym section
      * @const {integer}
      */
-    const gymSections = 30
+  const gymSections = 30
 
-    /**
+  /**
      * Open hours for the gym
      * @const {array}
      */
-    const openHours = [[8, 22.5]];
+  const openHours = [[8, 22.5]]
 
-    /**
+  /**
      * Open hours for the gym
      * @const {string}
      */
-    const url = 'http://localhost:8000/gymBookings/'
+  const url = 'http://localhost:8000/gymBookings/'
 
-    /**
+  /**
      * Open hours for the gym
      * @const {string}
      */
-    const maxGymSessionTime = 3
+  const maxGymSessionTime = 3
 
-    /**
+  /**
      * State for the currently booked gym times.
-     * 
+     *
      * @const {array}
      */
-    const [bookings, setBookings] = useState('')
+  const [bookings, setBookings] = useState('')
 
-    /**
+  /**
      * State wether to show the confirmation for the booking or not.
-     * 
+     *
      * @const {boolean}
      */
-    const [showConfirmation, setShowModal] = useState(false);
+  const [showConfirmation, setShowModal] = useState(false)
 
-    /**
+  /**
      * State which tells if the user has chosen a time or not.
-     * 
+     *
      * @const {boolean}
      */
-    const [hasChosenTime, setHasChosenTime] = useState(false)
+  const [hasChosenTime, setHasChosenTime] = useState(false)
 
-    /**
+  /**
      * State which holds the user picked for start time the gym session.
-     * 
+     *
      * @const {object}
      */
-    const [startTime, setStartTime] = useState('')
+  const [startTime, setStartTime] = useState('')
 
-    /**
+  /**
      * State which holds the user picked end time for the gym session.
-     * 
+     *
      * @const {object}
      */
-    const [endTime, setEndTime] = useState('')
+  const [endTime, setEndTime] = useState('')
 
-    /**
+  /**
      * Function to close the confirmationmodal
      */
-    const handleClose = () => setShowModal(false);
+  const handleClose = () => setShowModal(false)
 
-    /**
+  /**
      * Function to show the confirmationmodal
      */
-    const handleShow = () => setShowModal(true);
+  const handleShow = () => setShowModal(true)
 
-
-    /**
+  /**
      * Functinon to fetch the booked gymtimes from the database
      */
-    const fetchBookings = useCallback(async () => {
-        fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                parseData(json)
-            });
-    }, [])
+  const fetchBookings = useCallback(async () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        parseData(json)
+      })
+  }, [])
 
-    /**
+  /**
      * Functinon to parse the fetched data from the database
-     * 
-     * @param {object} bookings The fetched data from the database 
+     *
+     * @param {object} bookings The fetched data from the database
      */
-    const parseData = (bookings) => {
-        bookings.forEach((booking) => {
-            booking.end_time = JSON.stringify(addMinutes(new Date(booking.end_time), 1)).replace(/"/g, "")
-        })
-        console.log(bookings)
-        setBookings(bookings)
-    }
+  const parseData = (bookings) => {
+    bookings.forEach((booking) => {
+      booking.end_time = JSON.stringify(addMinutes(new Date(booking.end_time), 1)).replace(/"/g, '')
+    })
+    console.log(bookings)
+    setBookings(bookings)
+  }
 
-    /**
-     * React hook to fetch the 
+  /**
+     * React hook to fetch the
      */
-    useEffect(() => {
-        fetchBookings()
-    }, [fetchBookings])
+  useEffect(() => {
+    fetchBookings()
+  }, [fetchBookings])
 
-
-    //Creates a new booking
-    const newBooking = async (startTime, endTime) => {
-        if (startTime === '' || endTime === '') {
-            alert('Inga start eller sluttider valda')
-        } else {
-            const bookingData = {
-                start_time: startTime,
-                end_time: endTime,
-                apartmentNo: JSON.parse(localStorage.getItem('tokens')).apartmentNo
-            }
-            await postBooking(bookingData)
-            Emailer(bookingData, 'GYM')
-        }
-        await fetchBookings()
+  // Creates a new booking
+  const newBooking = async (startTime, endTime) => {
+    if (startTime === '' || endTime === '') {
+      alert('Inga start eller sluttider valda')
+    } else {
+      const bookingData = {
+        start_time: startTime,
+        end_time: endTime,
+        apartmentNo: JSON.parse(localStorage.getItem('tokens')).apartmentNo
+      }
+      await postBooking(bookingData)
+      Emailer(bookingData, 'GYM')
     }
+    await fetchBookings()
+  }
 
-    //Posts the previously created booking
-    const postBooking = async (postData) => {
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(postData)
-        };
-        const response = await fetch(url, requestOptions)
-        const data = await response.json()
-        console.log(data)
+  // Posts the previously created booking
+  const postBooking = async (postData) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postData)
     }
+    const response = await fetch(url, requestOptions)
+    const data = await response.json()
+    console.log(data)
+  }
 
-    // Handles the "book" button on the modal
-    const handleModalConfirmation = () => {
-        setShowModal(false);
+  // Handles the "book" button on the modal
+  const handleModalConfirmation = () => {
+    setShowModal(false)
 
-        //om bekräftat körs denna för att "spara bokningen"
-        newBooking(startTime, endTime)
+    // om bekräftat körs denna för att "spara bokningen"
+    newBooking(startTime, endTime)
 
-        clearTimeInterval()
-    }
+    clearTimeInterval()
+  }
 
-    /**
+  /**
      * Handles and decides if the picked time is a starttime or an endtime.
-     * 
-     * @param {Date} time Date-fns object 
+     *
+     * @param {Date} time Date-fns object
      */
-    const handleChosenTime = (time) => {
-
-        if (startTime === '') {
-            setStartTime(time)
-            setEndTime(time)
-        } else if (differenceInMinutes(time, startTime) > maxGymSessionTime * 60 || time < startTime) {
-            clearTimeInterval()
-        } else {
-            setEndTime(time)
-            setHasChosenTime(true)
-        }
+  const handleChosenTime = (time) => {
+    if (startTime === '') {
+      setStartTime(time)
+      setEndTime(time)
+    } else if (differenceInMinutes(time, startTime) > maxGymSessionTime * 60 || time < startTime) {
+      clearTimeInterval()
+    } else {
+      setEndTime(time)
+      setHasChosenTime(true)
     }
+  }
 
-    //Clears the chosen time interval
-    const clearTimeInterval = () => {
-        setHasChosenTime(false)
-        setStartTime('')
-        setEndTime('')
-    }
+  // Clears the chosen time interval
+  const clearTimeInterval = () => {
+    setHasChosenTime(false)
+    setStartTime('')
+    setEndTime('')
+  }
 
-    return (
-        <>
-            <h4 className="pt-4 pb-4 ml-auto mr-auto">Här bokar du dina gymtider</h4>
-            <div className="w-50 ml-auto mr-auto mb-4" ><ol className="instructionsList">
-                <li>Välj ett datum</li>
-                <li>Välj en starttid</li>
-                <li>Välj en sluttid (inom {maxGymSessionTime} timmar)</li>
-                <li>Klicka på boka vald tid</li>
-                <li>Bekräfta bokning i rutan som kommer upp</li>
-            </ol>
-            </div>
+  return (
+    <>
+      <h4 className='pt-4 pb-4 ml-auto mr-auto'>Här bokar du dina gymtider</h4>
+      <div className='w-50 ml-auto mr-auto mb-4'><ol className='instructionsList'>
+        <li>Välj ett datum</li>
+        <li>Välj en starttid</li>
+        <li>Välj en sluttid (inom {maxGymSessionTime} timmar)</li>
+        <li>Klicka på boka vald tid</li>
+        <li>Bekräfta bokning i rutan som kommer upp</li>
+                                                 </ol>
+      </div>
 
-            <Button className="mb-3 mr-5" disabled={!hasChosenTime} variant={hasChosenTime ? ("primary") : ("secondary")} onClick={handleShow} >
-                Boka markerad tid
-            </Button>
-            <Button variant="secondary" className="mb-3 ml-5" onClick={clearTimeInterval}>
-                Rensa markerad tid
-            </Button>
-            <div className="border-top">
-                <TimeCalendar
-                    clickable
-                    openHours={openHours}
-                    disableHistory
-                    timeSlot={gymSections}
-                    bookings={bookings}
-                    startTime={startTime}
-                    endTime={endTime}
-                    onTimeClick={handleChosenTime}
-                />
-            </div>
+      <Button className='mb-3 mr-5' disabled={!hasChosenTime} variant={hasChosenTime ? ('primary') : ('secondary')} onClick={handleShow}>
+        Boka markerad tid
+      </Button>
+      <Button variant='secondary' className='mb-3 ml-5' onClick={clearTimeInterval}>
+        Rensa markerad tid
+      </Button>
+      <div className='border-top'>
+        <TimeCalendar
+          clickable
+          openHours={openHours}
+          disableHistory
+          timeSlot={gymSections}
+          bookings={bookings}
+          startTime={startTime}
+          endTime={endTime}
+          onTimeClick={handleChosenTime}
+        />
+      </div>
 
-            {/* Modal for confirmation of booking*/}
-            <Modal show={showConfirmation} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Bekräfta din bokning</Modal.Title>
-                </Modal.Header>
-                {!hasChosenTime ? (<p>Var vänlig välj tider innan du bokar</p>) : (
-                    <Modal.Body>
-                        Bekräfta din bokning av gymtid.
-                        <br />
-                        Tid: {JSON.stringify(format(startTime, 'HH.mm')).replace(/"/g, "")} - {JSON.stringify(format(endTime, 'HH.mm')).replace(/"/g, "")}
-                        <br />
-                        Dag: {JSON.stringify(format(startTime, 'dd/MM-yyyy')).replace(/"/g, "")}
-                    </Modal.Body>
-                )}
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Stäng
-                    </Button>
-                    <Button variant="primary" onClick={handleModalConfirmation}>
-                        Boka
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    )
+      {/* Modal for confirmation of booking */}
+      <Modal show={showConfirmation} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Bekräfta din bokning</Modal.Title>
+        </Modal.Header>
+        {!hasChosenTime ? (<p>Var vänlig välj tider innan du bokar</p>) : (
+          <Modal.Body>
+            Bekräfta din bokning av gymtid.
+            <br />
+            Tid: {JSON.stringify(format(startTime, 'HH.mm')).replace(/"/g, '')} - {JSON.stringify(format(endTime, 'HH.mm')).replace(/"/g, '')}
+            <br />
+            Dag: {JSON.stringify(format(startTime, 'dd/MM-yyyy')).replace(/"/g, '')}
+          </Modal.Body>
+        )}
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Stäng
+          </Button>
+          <Button variant='primary' onClick={handleModalConfirmation}>
+            Boka
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
 }
