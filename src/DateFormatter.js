a730fad98f9dca704a3533aa22fce9ba3032b716
jonasn-chalmers <jonnordi@student.chalmers.se>
2021-05-19 12:54:16 +0200

    Docs added for DateFormatter

--- src/DateFormatter.js
+++ src/DateFormatter.js
@@ -1,6 +1,13 @@
 import { format} from 'date-fns'
 import { sv } from 'date-fns/esm/locale'
 
+
+/**
+ * Function to format a date to a human readable time
+ * 
+ * @param {String} dateString A date as a string to extract the time from in swedish locale 
+ * @returns 
+ */
 export const formatTime = (dateString) => {
     let date = new Date(dateString)
     let form = "H:mm"
@@ -8,6 +15,12 @@ export const formatTime = (dateString) => {
     return format(date, form, { locale: sv })
 }
 
+/**
+ * Function to format a date to a human readable day
+ * 
+ * @param {String} dateString A date as a string to extract the day from in swedish locale 
+ * @returns 
+ */
 export const formatDay = (dateString) => {
     let date = new Date(dateString)
     let form = "PPPP"
